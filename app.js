4ba5a579e9c672142522e87191cd54c51fcc87db
Nutifafa Attor <attorfafa@gmail.com>
2022-04-26 14:44:51 +0000

    character variables

---
+++ app.js
@@ -0,0 +1,10 @@
+// Characters
+const capital_alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const small_alphabets = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const special_characters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+
+// Function
+function generatePassword() {
+    var characters = capital_alphabets + small_alphabets + numbers + special_characters
+}
